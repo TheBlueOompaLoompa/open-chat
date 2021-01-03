3b6124e84d8b357f582ea7e3ee6615ba6d58ddf6
Benjamin Nack <ben@nackfamily.com>
2021-01-03 15:18:27 -0600

    Add user creation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,13 +4,13 @@ import * as admin from 'firebase-admin';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-admin.initializeApp();
-
-let db = admin.firestore();
-
 // HTTPS endpoint /gmpkey
 export const getUserPubKey = functions.https.onRequest((req, res) => {  // Allow message sender to request the key public key of the user with the email passed in
-    let userCollection = db.collection('users').limit(1);
+    admin.initializeApp();
+
+    let db = admin.firestore();
+
+    let userCollection = db.collection('users');
 
     let query = userCollection.where("email", "==", req.body.email);
 
@@ -20,4 +20,20 @@ export const getUserPubKey = functions.https.onRequest((req, res) => {  // Allow
             let docData = querySnapshot.docs[0].data;
             res.send({ publicKey: docData().publicKey, uid: querySnapshot.docs[0].id });
         });
+});
+
+export const setupUserDocment = functions.auth.user().onCreate(user => {
+    admin.initializeApp();
+
+    let db = admin.firestore();
+
+    let userCollection = db.collection('users');
+
+    if(user.emailVerified === true){
+        userCollection.doc(user.uid).create({
+            email: user.email,
+            icon: user.photoURL,
+            name: user.displayName
+        });
+    }
 });
